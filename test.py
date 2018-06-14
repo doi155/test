47798a404936848856ab5c13a1662f8754f0dde7
Munehiro Doi <doi@fixstars.com>
2018-06-14 12:32:46 +0900

    Use unimported func.

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import sys
 
+
+def foo():
+    print(os.path.cwd())
+
 def test():
     print('test desuyo', file=sys.stdout)  
-
+    print(foo())
+    
+    
 if __name__ == '__main__':
     test()
