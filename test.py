d2e2fd6b9c1ff2856d40cda783d3f1c1a250c98c
Munehiro Doi <doi@fixstars.com>
2018-06-14 12:36:45 +0900

    Fix pointed errors.

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ import sys
 def foo():
     print(os.path.cwd())
 
+
 def test():
-    print('test desuyo', file=sys.stdout)  
+    print('test desuyo', file=sys.stdout)
     print(foo())
-    
-var test = 0.5f
-    
+
+
 if __name__ == '__main__':
     test()
